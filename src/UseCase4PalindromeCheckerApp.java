7ec155540bf920a4af6838206a6f1c530386f44b
anushkaagr-2006 <agrawalanushkakumari944@gmail.com>
2026-03-12 14:42:39 +0530

    UC4 - Character array based palindrome validation

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------
+ * MAIN CLASS - UseCase4PalindromeCheckerApp
+ * -----------------------------------------------------------
+ *
+ * Use Case 4: Character Array Based Validation
+ *
+ * Description:
+ * This class validates a palindrome by converting
+ * the string into a character array and comparing
+ * characters using the two-pointer technique.
+ *
+ * At this stage, the application:
+ * - Converts string to char array
+ * - Uses start and end pointers
+ * - Compares characters efficiently
+ * - Displays the result
+ *
+ * @author Developer
+ * @version 4.0
+ */
+
+public class UseCase4PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC4
+     */
+    public static void main(String[] args) {
+
+        String input = "radar";
+
+        // Convert string to character array
+        char[] chars = input.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+
+    }
+}
